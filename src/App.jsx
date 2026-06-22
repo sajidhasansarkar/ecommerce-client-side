b162efa47c0155185a1b2e618d2f68f5a9de89cd
unknown <you@example.com>
2026-06-22 13:20:49 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-bold text-center' >Welcome to the Client Side of the Application!</h1>
+    </>
+  )
+}
+
+export default App
